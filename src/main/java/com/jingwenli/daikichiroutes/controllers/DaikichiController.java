1b459281c683c5ae95e55389921bb30a6d7e4bc4
Jingwen Li <lijingwenot@gmail.com>
2022-10-02 12:25:40 -0700

    Initial Commit

---
+++ src/main/java/com/jingwenli/daikichiroutes/controllers/DaikichiController.java
@@ -0,0 +1,41 @@
+package com.jingwenli.daikichiroutes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+
+	@RequestMapping("")
+	public String root() {
+		return "Welcome";
+	}
+	
+	@RequestMapping(value="/today", method = RequestMethod.GET)
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@GetMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@GetMapping("/travel/{place}")
+	public String travel(@PathVariable("place") String place) {
+		return "Congrats! You will soon travel to " + place;
+	}
+	
+	@GetMapping("/lotto/{number}")
+	public String numberPath(@PathVariable("number") Integer number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of temping offers.";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}	
+	}
+}
